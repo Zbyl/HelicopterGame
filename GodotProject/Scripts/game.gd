ae5867105e67c5748cbbda84072ceedc0961dc46
Zbigniew Skowron <zbychs@gmail.com>
2024-05-31 11:17:00 +0200

    Pausing.

--- GodotProject/Scripts/game.gd
+++ GodotProject/Scripts/game.gd
@@ -6,6 +6,7 @@ class_name Game
 const EXAMPLE_LEVEL = preload("res://Scenes/example_level.tscn")
 
 var level
+var death_camera: Camera3D = null # Camera we use after player died.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,15 +19,19 @@ func _process(delta):
 		hud.toggle_menu()
 
 func _on_pause_game(do_pause: bool):
-	print('Paused.' if do_pause else 'Unpaused.')
+	print('Pausing.' if do_pause else 'Unpausing.')
+	pause_player_and_enemies(do_pause)
 
 func _on_new_game_pressed():
 	#hud._on_new_game()
 	#await _switch_level(INTRO, false)
 	#await level.intro_ended
-	_switch_level(EXAMPLE_LEVEL, true)
-
+	await _switch_level(EXAMPLE_LEVEL, true)
+	
 func _switch_level(new_level_scene, show_weapons: bool):
+	if death_camera:
+		death_camera.queue_free()
+		death_camera = null
 	if level:
 		level.queue_free()
 		level = null
@@ -42,4 +47,21 @@ func _switch_level(new_level_scene, show_weapons: bool):
 	hud.show_background(false)
 	hud.show_menu(false)
 	#hud.show_weapons(show_weapons)
+	
+func pause_player_and_enemies(do_pause: bool):
+	var player = get_tree().get_first_node_in_group("Player")
+	if player:
+		player.pause(do_pause)
+	var enemies = get_tree().get_nodes_in_group("Enemies")
+	for enemy in enemies:
+		enemy.pause(do_pause)
+
+func _on_player_died(): # Called by Helicopter when its health drops to zero.
+	GameData.game.pause_player_and_enemies(true)
+	var player = get_tree().get_first_node_in_group("Player")
+	death_camera = player.get_node('%Camera')
+	death_camera.reparent(get_tree().root)
+	
+	await get_tree().create_timer(2).timeout
+	hud.show_menu(true)
 	
